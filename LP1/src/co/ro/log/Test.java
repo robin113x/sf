e27311064e316fcfeac221e890c5ba755a06f276
Robin Roy <robinr@amdocs.com>
2023-10-09 02:11:43 +0530

    Logging

---
+++ LP1/src/co/ro/log/Test.java
@@ -0,0 +1,13 @@
+package co.ro.log;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Logger logger = Logger.getLogger("Test");
+		logger.log(Level.SEVERE, "This Is very Important ...");
+		
+	}
+}
