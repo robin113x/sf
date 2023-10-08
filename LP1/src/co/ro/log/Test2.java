2375e31f29ff27ae14d95f6b410445918c3a831a
Robin Roy <robinr@amdocs.com>
2023-10-09 02:31:59 +0530

    Logging

--- LP1/src/co/ro/log/Test2.java
+++ LP1/src/co/ro/log/Test2.java
@@ -4,10 +4,16 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class Test2 {
 	public static void main(String[] args) throws SecurityException, IOException {
 		FileHandler filehander = new FileHandler("C:\\Users\\robinr\\Documents\\SpringCourse\\text.log");
+		
+		SimpleFormatter simpleformatter = new SimpleFormatter();
+		filehander.setFormatter(simpleformatter);
+		
+		
 		Logger logger = Logger.getLogger("Test2");
 		logger.addHandler(filehander);
 		logger.log(Level.SEVERE, "This Is very Important ...");
