fdc8708d34049e1b6b01f70229b66d53c1fee567
Robin Roy <robinr@amdocs.com>
2023-10-10 02:03:59 +0530

    INLN

--- INLN1/src/co/ro/main/Main2.java
+++ INLN1/src/co/ro/main/Main2.java
@@ -4,10 +4,16 @@ import java.util.Locale;
 
 public class Main2 {
 	public static void main(String[] args) {
-		String[]countryCode = Locale.getISOCountries();
-		for(String cCode : countryCode) {
+		String[] countryCode = Locale.getISOCountries();
+		for (String cCode : countryCode) {
 			Locale local = new Locale("", cCode);
-			System.out.println(cCode+" -> "+local.getDisplayCountry());
+			System.out.println(cCode + " -> " + local.getDisplayCountry());
+		}
+		String[] langArr = Locale.getISOLanguages();
+		for (String lan : langArr) {
+			Locale local = new Locale(lan);
+			
+			System.out.println(lan+" -> "+local.getDisplayLanguage());
 		}
 	}
 }
